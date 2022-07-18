1fc489040732bf4a689655d59f782343fdfc6479
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2022-07-18 12:13:40 +0900

    [ADD] 깃정리

---
+++ src/Baekjoon/수학/약수의합2.java
@@ -0,0 +1,12 @@
+package Baekjoon.수학;
+
+import java.util.Scanner;
+
+public class 약수의합2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        long n = scanner.nextLong();
+        long result = 0;
+
+    }
+}
